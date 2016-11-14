8c6a1a389584492bb08d2ee4492a4d7448c3c758
MarciaOrr <orrmar@swcp.com>
2016-11-14 18:48:57 +0000

    Moved footer css to separate file. changed places per page to 4

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.all.paginate(:page => params[:page], :per_page => 2).order('name ASC')
+    @places = Place.all.paginate(:page => params[:page], :per_page => 4).order('name ASC')
   end
 
   def new
